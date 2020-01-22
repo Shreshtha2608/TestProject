16bc3d5fcd926bd82df7b2bdf470f094444f2004
Shreshtha <shreshtha807@gmail.com>
2020-01-22 12:17:01 +0530

    demoprojectchanges

--- myApp/myApp/model/usermodel.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var userSchema = new Schema({
-    name:{type: String},
-    id:{type: Number},
-});
-
-module.exports = mongoose.model('usermodel',userSchema);
